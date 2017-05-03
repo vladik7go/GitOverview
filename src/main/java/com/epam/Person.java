36df4840e52838b2645a9c3f46368684692f565e
Ivan Filimonau <Ivan_Filimonau@epam.com>
2017-05-03 03:48:58 -0700

    adding class Person

---
+++ src/main/java/com/epam/Person.java
@@ -0,0 +1,33 @@
+package com.epam;
+
+/**
+ * Created by Ivan on 5/3/2017.
+ */
+public class Person extends gitoverview{
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
